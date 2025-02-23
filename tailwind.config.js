016f2b64fad970abfeacb7a698ec20eb2f8b9229
Gizem Merve Demir <gizemervedemir@gmail.com>
2025-02-23 23:27:25 +0300

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   darkMode: "class",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./pages/**/*.{js,jsx}",
   ],
   theme: {
     extend: {
